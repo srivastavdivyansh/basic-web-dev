9fce2a69f7da446c743854592deda11941f08f4f
srivastavdivyansh <srivastavdivyansh16@gmail.com>
2023-12-19 14:09:42 +0530

    added HTML,CSS & JS

---
+++ Calculator/script.js
@@ -0,0 +1,18 @@
+function sum(){
+    document.getElementById("a1").value=parseInt(document.getElementById("ab1").value)+parseInt(document.getElementById("a").value)
+} 
+function mult(){
+    document.getElementById("a1").value=(parseInt(document.getElementById("ab1").value))*(parseInt(document.getElementById("a").value))
+
+}
+
+function sub(){
+    document.getElementById("a1").value=(parseInt(document.getElementById("ab1").value))-(parseInt(document.getElementById("a").value))
+    
+}
+function div(){
+   
+    document.getElementById("a1").value=(parseInt(document.getElementById("ab1").value))/(parseInt(document.getElementById("a").value))
+        
+
+}
